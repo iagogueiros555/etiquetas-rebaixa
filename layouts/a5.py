86c107a3b7ea66664f17eb7bcb7d3b5ffb8dd18b
iagogueiros555 <94695250+iagogueiros555@users.noreply.github.com>
2026-08-02 16:35:52 -0300

    Update a5.py

--- layouts/a5.py
+++ layouts/a5.py
@@ -75,7 +75,7 @@ def desenhar_etiqueta_a5(c, item, x_base, y_base, col_w, row_h, scale):
 
         largura_rs_de = c.stringWidth("R$", "Arial-Black", tam_fonte_de)
         x_reais_de = x_de + largura_rs_de + (2.0 * mm * scale)
-        y_reais_de = y_primeira_linha - (30 * mm * scale)
+        y_reais_de = y_primeira_linha - (100 * mm * scale)
         c.drawString(x_reais_de, y_reais_de, reais_de_str)
 
         largura_reais_de = c.stringWidth(reais_de_str, "Arial-Black", tam_fonte_reais_de)
